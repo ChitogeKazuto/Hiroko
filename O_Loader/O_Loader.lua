5a6a8251e6c87f07e94d4e35f9961973cf63c72f
ChitogeKazuto <70831261+ChitogeKazuto@users.noreply.github.com>
2021-02-04 18:34:38 +0700

    Update

--- O_Loader/O_Loader.lua
+++
@@ -1,9 +0,0 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/ChitogeKazuto/Hiroko/master/Ui%20Lib.lua"))()
-
-local Players = game:GetService("Players")
-
-if CanDo then
-    print("True")
-else
-    Players.LocalPlayer:Kick("HAHAHA You Can't Load In Github")
-end
